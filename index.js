8f454f85a61b45063f330a90804f77e04ae0ec68
Aliya <aliyanawal2020@gmail.com>
2025-07-06 23:27:04 +0530

    Add .env for backend port and client URL, and configure dotenv

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ const express = require('express');
 const http = require('http');
 const cors = require('cors');
 const { Server } = require('socket.io');
+require("dotenv").config();
+
 
 const app = express();
 app.use(cors());
+const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000", // React frontend port
+    origin:  process.env.CLIENT_URL, 
     methods: ["GET", "POST"]
   }
 });
@@ -28,6 +31,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(5000, () => {
+server.listen(PORT, () => {
   console.log("Server listening on port 5000");
 });
